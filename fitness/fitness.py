3403ac45638410d86da22e0e9941f557c0961075
nt-hn <nathanmekuria34@gmail.com>
2023-12-06 10:57:58 +0900

    updated based on comment

--- fitness/fitness.py
+++ fitness/fitness.py
@@ -22,22 +22,25 @@ def generate_docstring(code):
     return response.choices[0].message.content
 
 def calculate_bleu_score(predictions, references):
-    original_tokens = predictions.split()
-    transformed_tokens = references.split()
+    original_tokens = references.split()
+    transformed_tokens = predictions.split()
     smoothing = SmoothingFunction().method1
     bleu_score = sentence_bleu([transformed_tokens], original_tokens, smoothing_function=smoothing)
     return bleu_score
 
 def extract_docstring_and_content(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        file_content = file.read()
+    docstring = ''
+    file_content_without_docstring = ''
+    if file_path is not None:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            file_content = file.read()
 
-    # Extracting docstring using regular expression
-    docstring_match = re.search(r'\"\"\"(.+?)\"\"\"', file_content, re.DOTALL)
-    docstring = docstring_match.group(1) if docstring_match else None
+        # Extracting docstring using regular expression
+        docstring_match = re.search(r'\"\"\"(.+?)\"\"\"', file_content, re.DOTALL)
+        docstring = docstring_match.group(1) if docstring_match else None
 
-    # Removing docstring from file content
-    file_content_without_docstring = file_content.replace(f'\"\"\"{docstring}\"\"\"', '') if docstring else file_content
+        # Removing docstring from file content
+        file_content_without_docstring = file_content.replace(f'\"\"\"{docstring}\"\"\"', '') if docstring else file_content
 
     return docstring, file_content_without_docstring
 
@@ -47,14 +50,12 @@ if __name__ == "__main__":
     code = "def addition(a, b): return a + b"
     doc_string = generate_docstring(code)
     reference = "this function returns the sum of 2 numbers"
-
     fitness = calculate_bleu_score(doc_string, reference)
+    print(f"Fitness: {fitness}")
 
     # method 2
     file_path = "./input.py"  # Replace with the actual path to your input.py file
-
     reference, code = extract_docstring_and_content(file_path)
     doc_string = generate_docstring(code)
     fitness = calculate_bleu_score(doc_string, reference)
-
     print(f"Fitness: {fitness}")
